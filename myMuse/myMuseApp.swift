45303ffcd709500db0909a7eb2dd6062a5cd074f
tuckerhoppe <tuckerhoppe22@gmail.com>
2023-10-06 15:18:54 -0600

    Initial Commit

---
+++ myMuse/myMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMuseApp.swift
+//  myMuse
+//
+//  Created by Tucker on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct myMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
